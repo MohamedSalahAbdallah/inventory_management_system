e3fc11b8a4b725c1a0758b9573964c7788046a25
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-08-20 15:03:30 +0300

    create migration files

---
+++ database/migrations/2024_08_20_112249_create_inventory_movements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_movements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->foreignId('related_order_id');
+            $table->enum('movement_type', ['inbound', 'outbound', 'adjustment']);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_movements');
+    }
+};
