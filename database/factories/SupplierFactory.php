c19178aa95ea190c1ecc5a12d3cc981051686f1d
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-28 21:22:55 +0300

    Added 'image' field to SupplierFactory definition with faker imageUrl generator.

--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -21,6 +21,7 @@ class SupplierFactory extends Factory
             'email' => $this->faker->unique()->safeEmail,
             'phone' => $this->faker->phoneNumber,
             'address' => $this->faker->address,
+            'image' => $this->faker->imageUrl(),
         ];
     }
 }
