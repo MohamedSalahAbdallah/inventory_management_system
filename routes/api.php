2c86c6b00d01d551bbd11066f28d315f002d5844
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-01 00:07:52 +0300

    "Removed anonymous function route for '/user' and removed 3 lines of code."

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
 
 
 Route::apiResource('roles', RoleController::class)->middleware('auth:sanctum');
