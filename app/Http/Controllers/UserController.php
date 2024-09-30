9cbf4d74226795dba09cee481be3ebeff205fa29
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-30 18:21:41 +0300

    "Updated validation rules for supplier creation"

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => 'required|string',
+            "email" => 'required|email|unique:users,email',
+            "password" => 'required|min:8|confirmed',
+            "role_id" => 'required|exists:roles,id'
+        ]);
+
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password),
+            "role_id" => $request->role_id
+        ]);
+
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            "name" => 'required|string',
+            "email" => 'required|email|unique:users,email',
+            "password" => 'required|min:8|confirmed',
+            "role_id" => 'required|exists:roles,id'
+        ]);
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
