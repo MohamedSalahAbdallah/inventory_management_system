802f68de6b6452c5053abbde40e71b2547a27b5d
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-01 19:42:08 +0300

    added purchase orders Crud controller

---
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseOrder;
+use Illuminate\Http\Request;
+
+class PurchaseOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return PurchaseOrder::with(['user', 'supplier', 'productPurchaseOrders.product'])->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|integer|exists:users,id',
+            'total' => 'required|numeric|min:1',
+            'status' => 'required|string|max:255',
+            'supplier_id' => 'required|numeric|exists:suppliers,id'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return PurchaseOrder::with(['user', 'supplier', 'productPurchaseOrders.product'])->findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'user_id' => 'required|integer|exists:users,id',
+            'total' => 'required|numeric|min:1',
+            'status' => 'required|string|max:255',
+            'supplier_id' => 'required|numeric|exists:suppliers,id'
+        ]);
+
+        $purchaseOrder = PurchaseOrder::findOrFail($id);
+        $purchaseOrder->update($request->all());
+        return $purchaseOrder;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $purchaseOrder = PurchaseOrder::findOrFail($id);
+        $purchaseOrder->delete();
+        return "Deleted Successfully";
+    }
+}
