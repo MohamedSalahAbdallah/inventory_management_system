02b0ed4b4e5cd68eb15385eec23cddecbaf64580
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-01 18:55:13 +0300

    "Updated image fields in ProductFactory and SupplierFactory to use a fixed URL from picsum.photos instead of Faker's imageUrl()"

--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -21,7 +21,7 @@ class SupplierFactory extends Factory
             'email' => $this->faker->unique()->safeEmail,
             'phone' => $this->faker->phoneNumber,
             'address' => $this->faker->address,
-            'image' => $this->faker->imageUrl(),
+            'image' => 'https://picsum.photos/800',
         ];
     }
 }
