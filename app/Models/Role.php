523492dde24e34ae82f45c7755b33680c60f386b
ahmedadelattya <ahmedadelattya2@gmail.com>
2024-10-08 19:39:25 +0300

    added the activity logs for the crud operations on Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,16 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory,LogsActivity;
 
     protected $fillable = [
         'name',
     ];
 
-
+     // Implement the getActivitylogOptions method
+     public function getActivitylogOptions(): LogOptions
+     {
+         return LogOptions::defaults()
+             ->logOnly(['name'])
+             ->logOnlyDirty()               
+             ->useLogName('Role')                
+             ->setDescriptionForEvent(fn(string $eventName) => "Role {$eventName}");  
+     }
+ 
 
     public function users(): HasMany
     {
