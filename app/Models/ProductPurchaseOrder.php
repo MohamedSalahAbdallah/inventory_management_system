036ef4dfe27f0f28216ebccbfc264f65630b01d6
ahmedadelattya <ahmedadelattya2@gmail.com>
2024-10-08 20:06:39 +0300

    added the activity logs for the crud operations on ProductPurchaseOrder model

--- app/Models/ProductPurchaseOrder.php
+++ app/Models/ProductPurchaseOrder.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class ProductPurchaseOrder extends Model
 {
-    use HasFactory;
+    use HasFactory,LogsActivity;
 
     protected $fillable = [
         'quantity',
@@ -17,6 +19,15 @@ class ProductPurchaseOrder extends Model
         'purchase_order_id',
     ];
 
+     // Implement the getActivitylogOptions method
+     public function getActivitylogOptions(): LogOptions
+     {
+         return LogOptions::defaults()
+             ->logOnly(['quantity','price','product_id','purchase_order_id'])
+             ->logOnlyDirty()               
+             ->useLogName('ProductPuchaseOrder')                
+             ->setDescriptionForEvent(fn(string $eventName) => "ProductPuchaseOrder {$eventName}");  
+     }
 
     public function product(): BelongsTo
     {
