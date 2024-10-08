df93163428a25bd2ee4763d9588b1294e780b8c3
ahmedadelattya <ahmedadelattya2@gmail.com>
2024-10-08 19:39:47 +0300

    added the activity logs for the crud operations on Supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Supplier extends Model
 {
-    use HasFactory;
+    use HasFactory,LogsActivity;
 
     protected $fillable = [
         'name',
@@ -17,6 +19,15 @@ class Supplier extends Model
         'address',
         'image'
     ];
+     // Implement the getActivitylogOptions method
+     public function getActivitylogOptions(): LogOptions
+     {
+         return LogOptions::defaults()
+             ->logOnly(['name','email','phone','address','image'])
+             ->logOnlyDirty()               
+             ->useLogName('Supplier')                
+             ->setDescriptionForEvent(fn(string $eventName) => "Supplier {$eventName}");  
+     }
 
     public function products(): HasMany
     {
