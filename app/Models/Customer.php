c491287937b801ee0019f17d36712c7b10c51c94
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-10 04:56:17 +0300

    "Added Spatie activity log trait and configured log options to Customer model"

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,15 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Customer extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
 
     protected $fillable = [
         'name',
         'phone'
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'phone'])
+            ->logOnlyDirty()
+            ->useLogName('Customer')
+            ->setDescriptionForEvent(fn(string $eventName) => "Customer {$eventName}");
+    }
 
     public function salesOrders()
     {
