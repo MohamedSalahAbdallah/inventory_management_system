7dd64f9d9f675c31c0add1effb034564cce38b20
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-11 06:11:57 +0300

    "Made 'image' column in 'users' table nullable."

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('role_id')->constrained("roles")->onDelete('cascade');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
